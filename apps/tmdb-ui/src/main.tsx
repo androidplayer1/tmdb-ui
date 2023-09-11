6914f8147bd03efdbb275496b3ee76ff2ad9893f
androidplayer1 <viktor.madane95@gmail.com>
2023-09-11 19:56:05 +0200

    support base path

--- apps/tmdb-ui/src/main.tsx
+++ apps/tmdb-ui/src/main.tsx
@@ -1,19 +1,30 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { RouterProvider } from '@tanstack/react-router';
-import { StrictMode } from 'react';
+import { FC, StrictMode, useMemo } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import { router } from './app/router';
 import { TmdbTheme } from './theme';
 
+const Main: FC = () => {
+  const basePath = useMemo(() => {
+    const baseUrl = import.meta.env.BASE_URL;
+
+    if (baseUrl === '/') return undefined;
+    return baseUrl;
+  }, []);
+
+  return (
+    <StrictMode>
+      <ChakraProvider theme={TmdbTheme}>
+        <RouterProvider router={router} basepath={basePath} />
+      </ChakraProvider>
+    </StrictMode>
+  );
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-root.render(
-  <StrictMode>
-    <ChakraProvider theme={TmdbTheme}>
-      <RouterProvider router={router} />
-    </ChakraProvider>
-  </StrictMode>
-);
+root.render(<Main />);
